b0f7242fc85a31802028db16c21aa31ae93040ba
bwgjoseph <josephgan@live.com.sg>
2022-02-07 22:15:09 +0800

    Initial commit
    https://start.spring.io/#!type=maven-project&language=java&platformVersion=2.6.3&packaging=jar&jvmVersion=17&groupId=com.bwgjoseph&artifactId=spring-boot-mvn-starter&name=spring-boot-mvn-starter&description=Spring%20Boot%20Maven%20Starter%20Project&packageName=com.bwgjoseph.spring-boot-mvn-starter&dependencies=devtools,lombok,configuration-processor,web,security,data-mongodb,amqp,validation,actuator,testcontainers,flapdoodle-mongo

---
+++ src/main/java/com/bwgjoseph/springbootmvnstarter/SpringBootMvnStarterApplication.java
@@ -0,0 +1,13 @@
+package com.bwgjoseph.springbootmvnstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMvnStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMvnStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bwgjoseph/springbootmvnstarter/SpringBootMvnStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bwgjoseph.springbootmvnstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMvnStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
