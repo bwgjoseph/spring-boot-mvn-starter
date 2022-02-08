c15a7f5fc530f700e1a8d6f17fb2f5d9d5a6cf84
bwgjoseph <josephgan@live.com.sg>
2022-02-09 00:35:37 +0800

    add spotless plugin and config
    uses google-java-format

--- src/main/java/com/bwgjoseph/springbootmvnstarter/SpringBootMvnStarterApplication.java
+++ src/main/java/com/bwgjoseph/springbootmvnstarter/SpringBootMvnStarterApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootMvnStarterApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootMvnStarterApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMvnStarterApplication.class, args);
+    }
 }
